e09c94f35779f3ab23459331d5c3eb6217acf9c6
Alopwer <protonikser@gmail.com>
2020-05-23 19:24:24 +0300

    styling added to preloader, icon was changed

--- src/components/common/Preloader.jsx
+++ src/components/common/Preloader.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import preloader from '../../assets/isFetching.svg';
+import s from './common.module.scss';
 
 const Preloader = () => {
-    return <div>
+    return <div className={s['preloader']}>
         <img src={preloader}/>
     </div>
 }
